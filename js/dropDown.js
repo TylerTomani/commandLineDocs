16615f20eaf00e6d6b8367dc689341e6d92d1f18
Tyler Tomani <ttomani@gmail.com>
2022-06-02 11:07:57 -0400

    first draft working

--- js/dropDown.js
+++ js/dropDown.js
@@ -1,32 +1,39 @@
-const dropDownTop = document.querySelectorAll(".drop-link-top")
-const main = document.querySelector("main")
-const mainContentDiv = document.getElementById("section-select")
+const dropDownTop = document.querySelectorAll(".drop-link-top");
+const main = document.querySelector("main");
+const mainContentDiv = document.getElementById("section-select");
+const asideSubList = document.querySelectorAll(".topic > ul > li > ul ");
 
 function hideTopics() {   
-    let topics = document.querySelectorAll("aside > .topic")
+    let topics = document.querySelectorAll("aside > .topic");
     topics.forEach(topic => {
         if(!topic.classList.contains("hide")){
-            topic.classList.add("hide")
+            topic.classList.add("hide");
         }
     })
 }
 hideTopics();
 
 function hideSubList(){
-    
+    asideSubList.forEach(subUL => {
+        if(!subUL.classList.contains("hide")){
+            subUL.classList.add("hide");
+        }
+    })
 }
+hideSubList()
 
 
 dropDownTop.forEach(dropTop => {
     dropTop.addEventListener("click",(e) =>{
-        e.preventDefault()
+        e.preventDefault();
         let idRef = e.target.getAttribute("href");
-        idRef = idRef.slice(1)
-        let selectId = document.getElementById(idRef)
+        idRef = idRef.slice(1);
+        let selectId = document.getElementById(idRef);
         // Toggle Side Bar
         if(selectId.classList.contains("hide")){
             hideTopics();   
-            main.classList.remove("hide")
+            // hideSubList() // I don't know if I want this to hide or not
+            main.classList.remove("hide");
             selectId.classList.remove("hide");
             
         } else {
@@ -35,13 +42,31 @@ dropDownTop.forEach(dropTop => {
             
         }
         // Focus on sidebar's first list element
-        let liFirst = selectId.querySelector("ul > li > a")
-        liFirst.focus()
-        // Hide sub list 
-        // console.log(selectId)
+        let liFirst = selectId.querySelector("ul > li > a");
+        liFirst.focus();
         
+        // toggle Aside sub topic links
 
+        let asideDropLinks = selectId.querySelectorAll("ul > li > a");
+        let toggle = false;
+        asideDropLinks.forEach(drop => {
+            drop.addEventListener("click",e => {
+                let parent = e.target.parentNode;
+                let subList = parent.querySelector("ul ");
+               
+                if(!toggle){
+                    // console.log(parent)
+                    console.log("add")
+                    hideSubList()
+                    subList.classList.remove("hide");
+                } else {
+                    subList.classList.add("hide")
+                    console.log("hide")
+                }
+                toggle = !toggle;
+            })
+        })
         
-    
+        
     })
 })
